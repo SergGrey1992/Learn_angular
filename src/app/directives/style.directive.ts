0bb2c4a2866d5060c7edc5e2a6c527e1a09c0196
SergGrey <sergdiag19@gmail.com>
2021-07-26 10:45:28 +0300

    Добавление событий

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,11 +1,21 @@
-import {Directive, ElementRef, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
 //ng g d style2 --skipTests
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
-    this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
+    //this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
     //elRef.nativeElement.style.color = 'red'
+  }
+
+  @HostListener('click', ['$event.target']) onClick (event: Event) {
+    //console.log(event)
+  }
+  @HostListener('mouseenter') onMouseEnter () {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red')
+  }
+  @HostListener('mouseleave') onMouseLeave () {
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
   }
 }

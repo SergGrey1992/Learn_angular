82834278f0b33418d1782cbf0c950d3065835997
SergGrey <sergdiag19@gmail.com>
2021-07-26 10:17:58 +0300

    add style.directive.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
-import {Component, Output} from '@angular/core';
-
-export interface Post {
-  title: string
-  text: string
-  id?: number
-}
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,19 +6,5 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  posts: Array<Post> = [
-    {title: 'Хочу выучить Angular компоненты', text: 'Учу клмпоненты', id: 1},
-    {title: 'Следующий блок', text: 'Будет много чего', id: 2},
-  ]
-
-  updatePosts(post: Post) {
-    this.posts.unshift(post)
-    //console.log('updatePosts', post)
-  }
-  removePost(id: number) {
-    console.log('id', id)
-    this.posts = this.posts.filter((post) => post.id !== id )
-  }
   constructor() {}
-
 }
---
+++ src/app/directives/style.directive.ts
@@ -0,0 +1,7 @@
+import {Directive} from "@angular/core";
+//ng g d style2 --skipTests
+@Directive({
+  selector: '[appStyle]'
+})
+export class StyleDirective {
+}

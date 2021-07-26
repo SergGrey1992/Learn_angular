814b241bbdf1e78b666a8ea0549ed78930b03c46
SergGrey <sergdiag19@gmail.com>
2021-07-26 10:25:39 +0300

    получение доступа до элемента

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,7 +1,11 @@
-import {Directive} from "@angular/core";
+import {Directive, ElementRef, Renderer2} from "@angular/core";
 //ng g d style2 --skipTests
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  constructor(private elRef: ElementRef, private renderer: Renderer2) {
+    this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
+    //elRef.nativeElement.style.color = 'red'
+  }
 }

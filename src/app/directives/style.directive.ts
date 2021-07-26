923b7fce4cf2077fd113de62664e27cce7c8c780
SergGrey <sergdiag19@gmail.com>
2021-07-26 11:00:11 +0300

    Параметры в дерективу

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, ElementRef, HostListener, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, Input, Renderer2} from "@angular/core";
 //ng g d style2 --skipTests
 @Directive({
   selector: '[appStyle]'
 })
 export class StyleDirective {
+  @Input('appStyle') color: string = 'blue';
+  @Input() dStyle: {border?: string, fontWeight?: string, borderRadius?: string}
+
   constructor(private elRef: ElementRef, private renderer: Renderer2) {
     //this.renderer.setStyle(elRef.nativeElement, 'color', 'blue')
     //elRef.nativeElement.style.color = 'red'
@@ -13,9 +16,15 @@ export class StyleDirective {
     //console.log(event)
   }
   @HostListener('mouseenter') onMouseEnter () {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red')
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', this.color)
+    this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', this.dStyle.fontWeight)
+    this.renderer.setStyle(this.elRef.nativeElement, 'border', this.dStyle.border)
+    this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', this.dStyle.borderRadius)
   }
   @HostListener('mouseleave') onMouseLeave () {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'blue')
+    this.renderer.setStyle(this.elRef.nativeElement, 'color', null)
+    this.renderer.setStyle(this.elRef.nativeElement, 'fontWeight', null)
+    this.renderer.setStyle(this.elRef.nativeElement, 'border', null)
+    this.renderer.setStyle(this.elRef.nativeElement, 'borderRadius', null)
   }
 }
